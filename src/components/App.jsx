81082b15ffc26b90be50609a142d29110b5501e6
serheihimself <sergm080@gmail.com>
2023-03-22 15:04:32 +0200

    task main commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,86 +1,156 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ConstactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList ';
 import { nanoid } from 'nanoid';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  componentDidMount() {
-    const saveContact = localStorage.getItem('contacts');
+export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    const saveContact = localStorage.getItem('key');
     if (saveContact !== null) {
       const parseContact = JSON.parse(saveContact);
-      this.setState({ contacts: parseContact });
+      return parseContact;
     }
-  }
+    return;
+  });
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      const adaptedData = JSON.stringify(this.state.contacts);
-      localStorage.setItem('key', adaptedData);
-    }
-  }
+  useEffect(() => {
+    window.localStorage.setItem('key', JSON.stringify(contacts));
+  }, [contacts]);
 
-  addContactsList = newContact => {
-    const { contacts } = this.state;
+  const addContactsList = newContact => {
     const trueFilter = contacts.some(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
     if (trueFilter) {
       return alert(`${newContact.name} is already in contacts.`);
     }
-    const contact = { ...newContact, id: nanoid() };
-    this.setState(prevState => ({
-      contacts: [contact, ...prevState.contacts],
-    }));
+    setContacts(prevState => {
+      return [...prevState, { ...newContact, id: nanoid() }];
+    });
   };
 
-  filterContacts = () => {
-    const { contacts, filter } = this.state;
+  const filterContacts = () => {
     const normalName = filter.toLowerCase();
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(normalName)
     );
   };
 
-  onFilterChange = ev => {
-    this.setState({
-      filter: ev.target.value,
-    });
+  const onFilterChange = evt => {
+    setFilter(evt.target.value);
   };
 
-  deletedContacts = nameId => {
-    this.setState({
-      contacts: this.state.contacts.filter(contact => contact.id !== nameId),
-    });
-  };
-
-  render() {
-    const filteredName = this.filterContacts();
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ConstactForm onSubmit={this.addContactsList} />
-        {this.state.contacts.length > 0 ? (
-          <>
-            <h2>Contacts</h2>
-            <Filter
-              value={this.state.filter}
-              onFilterChange={this.onFilterChange}
-            />
-            <ContactList
-              onFilterContacts={filteredName}
-              onChange={this.deletedContacts}
-            />
-          </>
-        ) : (
-          <h2>"Contact list is empty"</h2>
-        )}
-      </div>
+  const deletedContacts = nameId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== nameId)
     );
-  }
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ConstactForm onSubmit={addContactsList} />
+      {contacts.length > 0 ? (
+        <>
+          <h2>Contacts</h2>
+          <Filter value={filter} onFilterChange={onFilterChange} />
+          <ContactList
+            onFilterContacts={filterContacts()}
+            onChange={deletedContacts}
+          />
+        </>
+      ) : (
+        <h2>"Contact list is empty"</h2>
+      )}
+    </div>
+  );
 }
+
+// import React, { Component } from 'react';
+// import ConstactForm from './ContactForm/ContactForm';
+// import Filter from './Filter/Filter';
+// import ContactList from './ContactList/ContactList ';
+// import { nanoid } from 'nanoid';
+
+// export class App extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+// componentDidMount() {
+//   const saveContact = localStorage.getItem('key');
+//   if (saveContact !== null) {
+//     const parseContact = JSON.parse(saveContact);
+//     this.setState({ contacts: parseContact });
+//   }
+// }
+
+// componentDidUpdate(prevProps, prevState) {
+//   if (prevState.contacts !== this.state.contacts) {
+//     const adaptedData = JSON.stringify(this.state.contacts);
+//     localStorage.setItem('key', adaptedData);
+//   }
+// }
+
+// addContactsList = newContact => {
+//   const { contacts } = this.state;
+//   const trueFilter = contacts.some(
+//     contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+//   );
+//   if (trueFilter) {
+//     return alert(`${newContact.name} is already in contacts.`);
+//   }
+//   const contact = { ...newContact, id: nanoid() };
+//   this.setState(prevState => ({
+//     contacts: [contact, ...prevState.contacts],
+//   }));
+// };
+
+// filterContacts = () => {
+//   const { contacts, filter } = this.state;
+//   const normalName = filter.toLowerCase();
+//   return contacts.filter(({ name }) =>
+//     name.toLowerCase().includes(normalName)
+//   );
+// };
+
+// onFilterChange = ev => {
+//   this.setState({
+//     filter: ev.target.value,
+//   });
+// };
+
+// deletedContacts = nameId => {
+//   this.setState({
+//     contacts: this.state.contacts.filter(contact => contact.id !== nameId),
+//   });
+// };
+
+//   render() {
+//     const filteredName = this.filterContacts();
+// return (
+//   <div>
+//     <h1>Phonebook</h1>
+//     <ConstactForm onSubmit={this.addContactsList} />
+//     {this.state.contacts.length > 0 ? (
+//       <>
+//         <h2>Contacts</h2>
+//         <Filter
+//           value={this.state.filter}
+//           onFilterChange={this.onFilterChange}
+//         />
+//         <ContactList
+//           onFilterContacts={filteredName}
+//           onChange={this.deletedContacts}
+//         />
+//       </>
+//     ) : (
+//       <h2>"Contact list is empty"</h2>
+//     )}
+//   </div>
+// );
+//   }
+// }
