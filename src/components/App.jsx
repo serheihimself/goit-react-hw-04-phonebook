4584fedaf19969ee625dca4dce3954008d26ce5c
serheihimself <sergm080@gmail.com>
2023-03-25 12:43:39 +0200

    final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       const parseContact = JSON.parse(saveContact);
       return parseContact;
     }
-    return;
+    return [];
   });
   const [filter, setFilter] = useState('');
 
@@ -67,90 +67,3 @@ export default function App() {
     </div>
   );
 }
-
-// import React, { Component } from 'react';
-// import ConstactForm from './ContactForm/ContactForm';
-// import Filter from './Filter/Filter';
-// import ContactList from './ContactList/ContactList ';
-// import { nanoid } from 'nanoid';
-
-// export class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   };
-
-// componentDidMount() {
-//   const saveContact = localStorage.getItem('key');
-//   if (saveContact !== null) {
-//     const parseContact = JSON.parse(saveContact);
-//     this.setState({ contacts: parseContact });
-//   }
-// }
-
-// componentDidUpdate(prevProps, prevState) {
-//   if (prevState.contacts !== this.state.contacts) {
-//     const adaptedData = JSON.stringify(this.state.contacts);
-//     localStorage.setItem('key', adaptedData);
-//   }
-// }
-
-// addContactsList = newContact => {
-//   const { contacts } = this.state;
-//   const trueFilter = contacts.some(
-//     contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-//   );
-//   if (trueFilter) {
-//     return alert(`${newContact.name} is already in contacts.`);
-//   }
-//   const contact = { ...newContact, id: nanoid() };
-//   this.setState(prevState => ({
-//     contacts: [contact, ...prevState.contacts],
-//   }));
-// };
-
-// filterContacts = () => {
-//   const { contacts, filter } = this.state;
-//   const normalName = filter.toLowerCase();
-//   return contacts.filter(({ name }) =>
-//     name.toLowerCase().includes(normalName)
-//   );
-// };
-
-// onFilterChange = ev => {
-//   this.setState({
-//     filter: ev.target.value,
-//   });
-// };
-
-// deletedContacts = nameId => {
-//   this.setState({
-//     contacts: this.state.contacts.filter(contact => contact.id !== nameId),
-//   });
-// };
-
-//   render() {
-//     const filteredName = this.filterContacts();
-// return (
-//   <div>
-//     <h1>Phonebook</h1>
-//     <ConstactForm onSubmit={this.addContactsList} />
-//     {this.state.contacts.length > 0 ? (
-//       <>
-//         <h2>Contacts</h2>
-//         <Filter
-//           value={this.state.filter}
-//           onFilterChange={this.onFilterChange}
-//         />
-//         <ContactList
-//           onFilterContacts={filteredName}
-//           onChange={this.deletedContacts}
-//         />
-//       </>
-//     ) : (
-//       <h2>"Contact list is empty"</h2>
-//     )}
-//   </div>
-// );
-//   }
-// }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,53 +1,59 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class ConstactForm extends Component {
-  state = { name: '', number: '' };
+export default function ConstactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = ev => {
-    const { name, value } = ev.target;
-    this.setState({
-      [name]: value,
-    });
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
-    this.props.onSubmit(this.state);
-    this.setState({ name: '', number: '' });
+    onSubmit({ name, number });
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          value={this.state.name}
-          onChange={this.handleChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <label htmlFor="number">Number</label>
-        <input
-          type="tel"
-          name="number"
-          value={this.state.number}
-          onChange={this.handleChange}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-        <button type="submit">Add contact</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <label htmlFor="number">Number</label>
+      <input
+        type="tel"
+        name="number"
+        value={number}
+        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <button type="submit">Add contact</button>
+    </form>
+  );
 }
 
 ConstactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
-
-export default ConstactForm;
